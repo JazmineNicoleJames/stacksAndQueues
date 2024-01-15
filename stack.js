5d0a524e8fd63c95f7b86b00b3a8ef56b589d8ff
Jazmine James <jazminjames92@gmail.com>
2024-01-14 19:06:37 -0800

    Stacks and queues exercise.

---
+++ stack.js
@@ -0,0 +1,73 @@
+/** Node: node for a stack. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** Stack: chained-together nodes where you can
+ *  remove from the top or add to the top. */
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /** push(val): add new value to end of the stack. Returns undefined. */
+
+  push(val) {
+    const newNode = new Node(val)
+
+    if(!this.first){
+      this.last = newNode;
+      this.first = newNode;
+    } else {
+    let current = this.first;
+    this.first = newNode;
+    this.first.next = current;
+    }
+    this.size++;
+}
+
+  /** pop(): remove the node from the top of the stack
+   * and return its value. Should throw an error if the stack is empty. */
+
+  pop() {
+    if(!this.first) {
+      throw new Error;
+    }
+    let removed = this.first;
+
+    this.first = this.first.next;
+    this.size--;
+    return removed.val;
+
+  }
+
+  /** peek(): return the value of the first node in the stack. */
+
+  peek() {
+    
+    if(this.first){
+      return this.first.val;
+    }
+  }
+
+  /** isEmpty(): return true if the stack is empty, otherwise false */
+
+  isEmpty() {
+    if(!this.first) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+}
+
+
+module.exports = Stack;
